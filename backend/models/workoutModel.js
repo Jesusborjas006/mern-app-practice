182abdaee1c0c194f43a22b9c9d054eb89db2bc8
Jesus Borjas <jesusbor006@gmail.com>
2023-06-30 16:52:55 -0500

    Remove.env

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+// timestamps give us a time for when the document was created
+
+module.exports = mongoose.model("Workout", workoutSchema);
